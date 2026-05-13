d1c386b083b488d2774800fc80f673592eddb617
haadhee sheeraz <haadheesheeraz2004@gmail.com>
2026-05-14 04:48:02 +0500

    Fix path to index.html in dashboard route

--- app/src/routes/dashboard.js
+++ app/src/routes/dashboard.js
@@ -3,7 +3,7 @@ const path = require('path');
 const router = express.Router();
 
 router.get('/dashboard', (req, res) => {
-    res.sendFile(path.join(__dirname, '../../dashboard/index.html'));
+    res.sendFile(path.join(__dirname, '../dashboard/index.html'));
 });
 
 module.exports = router;
